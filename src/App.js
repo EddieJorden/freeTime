4dfa0453cbce7a3056a95ea729bf3c7addbda908
Eddie Jorden <eddiejorden.tech@gmail.com>
2021-01-23 07:17:31 -0800

    attempted some css but at least i get to practice github

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-
 function App() {
 	return (
-		<div className="App">
-			<h1>free Time</h1>
+		<div>
+			<head>freeTime</head>
+			<div>
+				<h1>freeTime</h1>
+			</div>
+			<p>how much is your time worth?</p>
 		</div>
 	);
 }
